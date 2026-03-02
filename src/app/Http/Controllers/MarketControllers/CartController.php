99be18296931eab4923e088ce47fc194be70275a
Danil <danil.prozoroff2018@yandex.ru>
2026-03-02 23:15:44 +0300

    Create controllers and base routes for login/register

---
+++ src/app/Http/Controllers/MarketControllers/CartController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\MarketControllers;
+
+use App\Http\Controllers\Controller;
+
+class CartController extends Controller
+{
+    //
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\AuthControllers\AuthController;
+use App\Http\Controllers\AuthControllers\UserController;
+use App\Http\Controllers\MarketControllers\CartController;
+use App\Http\Controllers\MarketControllers\OrderController;
+use App\Http\Controllers\MarketControllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+
+// Views
+Route::get('/cart',[CartController::class,'index'])->name('cart.index');
+Route::get('/login',[AuthController::class,'loginView'])->name('auth.login');
+Route::get('/register',[AuthController::class,'registerView'])->name('auth.register');
+
+// POST Requests
+Route::post('/register', [AuthController::class, 'register'])->name('auth.registerRequest');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.loginRequest');
+
+Route::middleware('jwt')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
