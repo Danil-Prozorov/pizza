ec7e51d667c23a9818efd6503d3883034521b746
Danil <danil.prozoroff2018@yandex.ru>
2026-03-03 22:40:37 +0300

    Class: Created a new seeder class for products

---
+++ src/database/seeders/ProductSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $images = [ 'img/pizza_plus.png','img/italian_pizza.jpg','/img/pizza_cheese.jpg','img/pizza_burger.png' ];
+
+        $count = 0;
+        while( $count != 49 ){
+            DB::table( 'products' )->insert([
+                'name' => Str::random(10),
+                'image' => $images[rand(0,3)],
+                'price' => rand(100, 500),
+                'description' => Str::random(150),
+                'recipe' => Str::random(50),
+                'short_desc' => Str::random(50),
+                'category' => rand(1,2),
+                'stock' => rand(1,15),
+                'active' => rand(0,1),
+                'status' => rand(0,3),
+            ]);
+            $count++;
+        }
+
+        DB::table( 'product_statuses' )->insert([
+            'status_name' => 'active',
+        ]);
+
+        DB::table( 'product_statuses' )->insert([
+            'status_name' => 'inactive',
+        ]);
+
+        DB::table( 'product_statuses' )->insert([
+            'status_name' => 'sold out',
+        ]);
+    }
+}
